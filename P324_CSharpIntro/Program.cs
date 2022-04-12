380e3d8aeaf6dbc5cba61704b1eb2cf4fd49911e
Javid-C <javidia@code.edu.az>
2022-04-12 21:53:36 +0400

    Initial commit

---
+++ P324_CSharpIntro/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+
+namespace P324_CSharpIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //byte number = 5;
+            //short ShortNumber = 10; // Pascal case
+            //short shortNumberTest = 20; // Camel case
+            //short short_number = 30; // Sneake case
+
+            ////byte nubmer2 = 256;
+            //int number3 = 157893;
+            //long number4 = 9999999999;
+
+            //float numberFloat = 54.5f;
+            //double numberDouble = 54.5;
+            //decimal numberDecimal = 54.5m;
+
+            //bool result = false;
+
+            //char letter = 'a';
+
+            //string word = "P";
+
+            //DateTime date = new DateTime(2022,4,12);
+
+            //object obj = "string";
+            //object obj1 = 5;
+            //object obj2 = 45.4;
+            //object obj3 = DateTime.Now;
+
+            //int number = 12;
+            //int number2 = 12;
+            ////int total = number % number2;
+
+            //number += 5; // number = number + 5
+            //number -= 5;
+
+            //bool result1 = true;
+            //bool result2 = false;
+
+            ////Console.WriteLine(!result1);
+
+
+            //if (number>12)
+            //{
+            //    Console.WriteLine("Conditionin neticesi truedir");
+            //}else if (number == 12)
+            //{
+            //    Console.WriteLine("number 12ye beraberdir");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Number 12den boyuk deyil");
+            //}
+
+
+            //if(!result1)
+            //{
+            //    Console.WriteLine("result true");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("result false");
+            //}
+
+            //Console.WriteLine("Zehmet olmasa adinizi daxil edin");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Zehmet olmasa soy adinizi daxil edin");
+            //string surname = Console.ReadLine();
+
+            //string fullname = name + " " + surname;
+            //Console.WriteLine(fullname);
+
+
+            Console.WriteLine("Reqem daxil edin");
+            int num = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine(num);
+
+
+            //switch (day)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Monday");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Tuesday");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Wednesday");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Thursday");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Friday");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Day is in incorrect format");
+            //        break;
+
+            //}
+
+            int day = 15;
+
+            //switch (day)
+            //{
+            //    case 1:
+            //    case 2:
+            //    case 20:
+            //    case 30:
+            //    case 31:
+            //        Console.WriteLine("January");
+            //        break;
+
+            //    case 64:
+            //    case 70:
+            //    case 80:
+            //        Console.WriteLine("Mart");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Incorrect format");
+            //        break;
+            //}
+
+            
+
+            //var variable = 5;
+            ////variable = "Fakhri"; //error
+
+            //dynamic age = 20;
+            //age = "Isa";
+
+            //age = 'a';
+            //Console.WriteLine(age);
+
+
+
+
+        }
+    }
+}
